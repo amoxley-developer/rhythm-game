2ec010514f565330e706513e1946cdf86e3e622f
amoxley <alex.moxley.dev@gmail.com>
2026-02-24 20:38:06 -0800

    add the label

--- scripts/GameLevel/game_level.gd
+++ scripts/GameLevel/game_level.gd
@@ -1,13 +1,16 @@
 class_name GameLevel
 extends Node2D
 
-@onready var NeonMarshPlayerScene: AudioStreamPlayer2D = $NeonMarshPlayer; 
-@onready var MetronomeScene: Metronome = $Metronome;
+@onready var NeonMarshPlayerScene: AudioStreamPlayer2D = $NeonMarshPlayer 
+@onready var MetronomeScene: Metronome = $Metronome
+# This is only temporary
+@onready var LabelScene: RichTextLabel = $RichTextLabel
 
 var current_beat: int
 
 func _ready() -> void:
 	current_beat = MetronomeScene.current_beat
+	LabelScene.text = str(current_beat)
 	MetronomeScene.beat_event.connect(_on_beat_event_signal);
 
 func _process(delta: float) -> void:
@@ -20,5 +23,5 @@ func _process(delta: float) -> void:
 
 func _on_beat_event_signal(new_current_beat: int) -> void:
 	current_beat = new_current_beat
-	print(current_beat)
+	LabelScene.text = str(current_beat)
 	
--- scripts/Metronome/metronome.gd
+++ scripts/Metronome/metronome.gd
@@ -2,7 +2,7 @@ class_name Metronome
 extends Node2D
 
 signal beat_event(current_beat: float)
-const BPM := 132
+const BPM := 132 * 2
 const SECONDS_PER_BEAT := 60.0/BPM 
 var current_beat := 0
 var time_until_next_beat := SECONDS_PER_BEAT
