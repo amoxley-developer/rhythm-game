b16bcb7b9cce9b3985cfb3484ae73d9bdda4c5f4
amoxley <alex.moxley.dev@gmail.com>
2026-02-24 20:13:43 -0800

    add beat event emission

--- scripts/GameLevel/game_level.gd
+++ scripts/GameLevel/game_level.gd
@@ -2,12 +2,23 @@ class_name GameLevel
 extends Node2D
 
 @onready var NeonMarshPlayerScene: AudioStreamPlayer2D = $NeonMarshPlayer; 
+@onready var MetronomeScene: Metronome = $Metronome;
+
+var current_beat: int
+
+func _ready() -> void:
+	current_beat = MetronomeScene.current_beat
+	MetronomeScene.beat_event.connect(_on_beat_event_signal);
 
 func _process(delta: float) -> void:
-  if Input.is_action_just_pressed('ui_select'):
-    if !NeonMarshPlayerScene.playing:
-      NeonMarshPlayerScene.play()
-    else:
-      print(NeonMarshPlayerScene.get_playback_position())
-      NeonMarshPlayerScene.stop()
-      
+	if Input.is_action_just_pressed('ui_select'):
+		if !NeonMarshPlayerScene.playing:
+			NeonMarshPlayerScene.play()
+
+	if NeonMarshPlayerScene.playing:
+		MetronomeScene.update_beat(NeonMarshPlayerScene.get_playback_position())
+
+func _on_beat_event_signal(new_current_beat: int) -> void:
+	current_beat = new_current_beat
+	print(current_beat)
+	
