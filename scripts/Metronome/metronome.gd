0cc8282190b328e15c67690e142fff5b0a801ee8
amoxley <alex.moxley.dev@gmail.com>
2026-02-25 20:40:16 -0800

    add active beat function

--- scripts/Metronome/metronome.gd
+++ scripts/Metronome/metronome.gd
@@ -4,6 +4,7 @@ extends Node2D
 signal beat_event(current_beat: float)
 const BPM := 132 * 2
 const SECONDS_PER_BEAT := 60.0/BPM 
+const ACTIVE_WINDOW := 0.08
 var current_beat := 0
 var time_until_next_beat := SECONDS_PER_BEAT
 
@@ -13,4 +14,8 @@ func update_beat(current_time: float) -> void:
     beat_event.emit(current_beat)
     time_until_next_beat += SECONDS_PER_BEAT
 
-
+func active_beat(current_time: float) -> int:
+  if current_time - (time_until_next_beat - SECONDS_PER_BEAT) <= ACTIVE_WINDOW or time_until_next_beat - current_time <= ACTIVE_WINDOW:
+    return current_beat
+  # returning negative 1 because of typing reasons and the current beat can't be -1
+  return -1 
